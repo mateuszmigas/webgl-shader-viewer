bc32f20a5dde89f564c7b8959c83b536d0327d22
Mateusz Migas <mateuszmigas.dev@gmail.com>
2021-03-21 21:51:16 +0100

    one webpack config

--- build/node-extension.webpack.config.js
+++ build/node-extension.webpack.config.js
@@ -1,38 +1,26 @@
-//@ts-check
-
-'use strict';
-
 const path = require('path');
 
-/**@type {import('webpack').Configuration}*/
-const config = {
-  target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-	//mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-  entry: {
-    'dist/extension': './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
-    'media/main': './src/viewer/src/index.tsx'
-  },
+const extensionConfig = {
+  target: 'node',
+	mode: 'none',
+  entry: './src/extension.ts',
   output: {
-    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, '..'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, '..', 'dist'),
+    filename: 'extension.js',
     libraryTarget: 'commonjs2',
     devtoolModuleFilenameTemplate: '../[resource-path]'
   },
-  //devtool: 'source-map',
-  devtool: "inline-source-map",
-  mode: "production",
+  devtool: 'source-map',
   externals: {
-    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    vscode: 'commonjs vscode'
   },
   resolve: {
-    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: ['.ts', '.js']
   },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts$/,
         exclude: /node_modules/,
         use: [
           {
@@ -43,4 +31,27 @@ const config = {
     ]
   }
 };
-module.exports = config;
+
+const viewerConfig =  {
+  entry: './src/viewer/src/index.tsx',
+  devtool: "inline-source-map",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '..', 'media')
+  },
+};
+
+module.exports = [viewerConfig, extensionConfig ];
